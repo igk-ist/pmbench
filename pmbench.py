17bd918a96f3a22002476ebdc53758593ca219fc
Ilya Kuzmin <i.g.kuzmin.spb@gmail.com>
2021-06-24 07:02:01 +0300

    current stage

--- pmbench.py
+++ pmbench.py
@@ -1,3 +1,5 @@
+from utils import * #concat pool, rw_ratio, ws, threads, merge_ratios
+
 # %%
 """
 # Hardware Details 
@@ -47,37 +49,6 @@ CACHE: lvl 3, 22528 KB, sets:32768, linesz:64, part:1, ways:11
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-def concat(fields, fd):
-    return fd[fields].T.values
-
-def pool(name, df):
-     return df[df['pool'] == name].drop(columns=["pool"])
-
-def rw_ratio(ratio, df):
-    if type(ratio) == str:
-        if   ratio == 'wo' : v = 0
-        elif ratio == 'rww': v = 33
-        elif ratio == 'rw' : v = 50
-        elif ratio == 'rrw': v = 66
-        elif ratio == 'ro' : v = 100
-        else: raise Exception('Unknown ratio "{}"'.format(ratio))
-    else:
-        v = ratio
-    return df[df['rw_ratio'] == v].drop(columns=["rw_ratio"])
-
-def ws(v, df):
-    return df[df['ws'] == v].drop(columns=['ws'])
-
-def threads(i, df):
-    return df[df['threads'] == i].drop(columns=['threads'])
-
-def merge_ratios(fd, ratios=['ro','rrw', 'rw', 'rww', 'wo']):
-    m = fd["threads"].drop_duplicates()
-    for n in ratios:
-        t = rw_ratio(n, fd).rename(columns={'ltc':n,'ltc_d':n+'_d'})
-        m = pd.merge(m, t)
-    return m
 
 # %%
 data = pd.read_csv('pmbench.csv', delimiter = ",")
@@ -196,7 +167,7 @@ def plot_mem100_wsz(wsz, ax):
                 label = n,
             )
     ax.legend(loc='upper left')
-        
+
 fig = plt.figure(figsize=(16,8))
 fig.suptitle('Threads saturation', fontsize=16)
 ax = fig.subplots(1, 2)
